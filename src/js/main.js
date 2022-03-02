21658861592791d43434ef3c8e2e556289023050
David <davrodrila@gmail.com>
2022-03-02 16:59:57 +0100

    Finished guessing feature

--- src/js/main.js
+++ src/js/main.js
@@ -32,28 +32,59 @@ const lettersNumbers = {
     "z": 25
 }
 
-const results = [4, 12, 9, 23, 15, 22];
 
 
 window.onload = function () {
     document.getElementById('boton').onclick = solve;
 }
 
-function calculateResults() {
-    const testEncrypt = 'wordle';
-    const testResult = 'aaaaaa';
-}
-
 function solve(e) {
+    const results = [4, 12, 9, 23, 15, 22];
+    const maxSize = numbersLetters.length;
     const worldeUrl = document.forms[0]['url'].value;
-    console.log(worldeUrl);
     const word = getWordParameter(worldeUrl);
-    console.log(word);
+    if (typeof word !== "string" || word.length <= 0) {
+        alert("URL invalida");
+        return false;
+    }
+    let solvedWord = '';
+    let resultOffset = 0;
+    console.log(results);
+    try {
+        for (let i = 0; i < word.length; i++) {
+            console.log(i);
+            console.log(word);
+            let encryptedCharacter = word[i];
+            let attemptedDistance = results[resultOffset] + lettersNumbers[encryptedCharacter];
+            console.log(encryptedCharacter);
+            console.log(attemptedDistance);
+            if (attemptedDistance > maxSize) {
+                attemptedDistance = attemptedDistance % maxSize;
+            } else if (attemptedDistance === maxSize) {
+                attemptedDistance = 0;
+            }
+
+            solvedWord += numbersLetters[attemptedDistance];
+            console.log(solvedWord);
+            resultOffset++;
+            if (resultOffset >= results.length) {
+                resultOffset = 0;
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+
+    document.getElementById('palabra').innerHTML = solvedWord;
+    let contenedor = document.getElementById('contenedorAdivinacion');
+    contenedor.classList.remove('hide');
+    contenedor.classList.add('show');
+
     e.preventDefault();
 }
 
 function getWordParameter(url) {
-
     var regex = new RegExp('[?&]word(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
     if (!results) return null;
